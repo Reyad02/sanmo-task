36b83ef3f9ea69b7789c6130cee288b99fd012f3
unknown <areyad203011@bscse.uiu.ac.bd>
2025-06-04 11:01:32 +0600

    faq part done

---
+++ js/script.js
@@ -0,0 +1,9 @@
+ document.querySelectorAll('.faq-item').forEach(item => {
+    item.addEventListener('click', () => {
+      document.querySelectorAll('.faq-item').forEach(i => {
+        if (i !== item) i.classList.remove('active');
+      });
+
+      item.classList.toggle('active');
+    });
+  });
